074e111abf5347a8a74755dd99b0cc9e3c6614e9
MieciuBombolini <lukaskrzyszewski@gmail.com>
2023-12-01 18:10:09 +0100

    Added comments to AoCUtils

--- src/main/java/dev/bebomny/Main.java
+++ src/main/java/dev/bebomny/Main.java
@@ -5,6 +5,7 @@ import dev.bebomny.challenges.Day02;
 
 public class Main {
     public static void main(String[] args) {
+        //new Day01();
         new Day02();
     }
 
--- src/main/java/dev/bebomny/utils/AoCUtils.java
+++ src/main/java/dev/bebomny/utils/AoCUtils.java
@@ -7,14 +7,21 @@ import java.util.List;
 public abstract class AoCUtils {
 
     private int part = 1;
+
+    //The start time in nanoseconds
     private long timerStart;
 
     public AoCUtils(String day) {
+
+        //Checks if it has a new file available in the "resources" folder for each day
+        //That file should contain that days input data
         File dayInputFile = new File("src/main/resources/day" + day + ".txt");
+
+        //If that file doesn't exist it runs it with an empty ArrayList
         if(!dayInputFile.exists()) {
             System.out.println("File does not exist!");
-            timerStart = System.nanoTime();
-            solve(new ArrayList<>());
+            timerStart = System.nanoTime(); // start measuring time
+            solve(new ArrayList<>()); // runs the algorithm implemented in the solve method with an empty ArrayList
             return;
         }
 
@@ -23,10 +30,12 @@ public abstract class AoCUtils {
             reader = new BufferedReader(new FileReader(dayInputFile));
         } catch (FileNotFoundException e) {
             System.err.println("File Not Found!");
-            solve(new ArrayList<>());
+            timerStart = System.nanoTime(); // start measuring time
+            solve(new ArrayList<>()); // runs the algorithm implemented in the solve method with an empty ArrayList
             return;
         }
 
+        //If the file exists it put each line as String into an ArrayList
         List<String> inputLines = new ArrayList<>();
         try {
             String line;
@@ -36,10 +45,11 @@ public abstract class AoCUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        timerStart = System.nanoTime();
-        solve(inputLines);
+        timerStart = System.nanoTime(); // start measuring time
+        solve(inputLines); // runs the algorithm with the ArrayList as input
     }
 
+    //In this method each class that extends this one should have the solving algorithm
     public abstract void solve(List<String> input);
 
     public void lap(int answer){
@@ -50,6 +60,7 @@ public abstract class AoCUtils {
         lap(String.valueOf(answer));
     }
 
+    //Records the time it took to run the algorithm and displays the answer with time it took to solve
     public void lap(String answer) {
         long timeSpent = (System.nanoTime() - timerStart) / 1000;
         System.out.println("Part " + part + ": " + answer + ", Duration: " + timeToString(timeSpent));
@@ -57,6 +68,7 @@ public abstract class AoCUtils {
         part++;
     }
 
+    //Converting nanosecond time into a more readable form
     public String timeToString(long timeSpent){
         if(timeSpent < 1000)
             return timeSpent + "μs";
@@ -65,6 +77,7 @@ public abstract class AoCUtils {
         return (timeSpent / 1000000.0) + "s";
     }
 
+    //Converts and ArrayList of Strings into an ArrayList of Integers
     public List<Integer> convertToInts(List<String> input){
         List<Integer> ints = new ArrayList<>();
         for(String s : input)
@@ -72,6 +85,7 @@ public abstract class AoCUtils {
         return ints;
     }
 
+    //Converts and ArrayList of Strings into an ArrayList of Longs
     public List<Long> convertToLongs(List<String> input){
         List<Long> longs = new ArrayList<>();
         for(String s : input)
